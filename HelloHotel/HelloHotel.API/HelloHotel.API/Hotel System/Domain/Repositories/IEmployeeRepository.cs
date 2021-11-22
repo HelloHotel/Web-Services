64df6b6d375762cb042d0ce7017c943aea3e54ff
Miner <54141767+Apolosmllb@users.noreply.github.com>
2021-11-22 03:39:24 -0500

    Update IEmployeeRepository.cs
    employee controller

--- HelloHotel/HelloHotel.API/HelloHotel.API/Hotel System/Domain/Repositories/IEmployeeRepository.cs
+++ HelloHotel/HelloHotel.API/HelloHotel.API/Hotel System/Domain/Repositories/IEmployeeRepository.cs
@@ -1,1 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HelloHotel.API.Hotel_System.Domain.Models;
 
+namespace HelloHotel.API.Hotel_System.Domain.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        Task<IEnumerable<Employee>> ListAsync();
+        Task AddAsync(Employee employee);
+        Task<Employee> FindByIdAsync(int id);
+        void Update(Employee employee);
+        void Remove(Employee employee);
+    }
+}
